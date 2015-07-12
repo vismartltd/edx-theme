76681ad55d98289922dbf10538576505308cc4c0
Alexander Khovansky <alx.khovansky@gmail.com>
2015-07-12 19:28:17 +0300

    Start implementing pages

--- updateTheme.sh
+++ updateTheme.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-echo "edX fast lms theme update script v.0.1\n"
-echo "Crafted at Nine'n'One HQ in Saint-Petersburg, Russia\n"
-echo "Pulling theme and updating assets\n"
+echo "edX fast lms theme update script v.0.1"
+echo "Crafted at Nine'n'One HQ in Saint-Petersburg, Russia"
+echo "Pulling theme and updating assets"
 sudo -H -u edxapp bash -c 'git pull; source /edx/app/edxapp/edxapp_env;cd /edx/app/edxapp/edx-platform;paver update_assets lms --settings=aws'
 
-echo "Removing cached templates\n"
+echo "Removing cached templates"
 sudo rm -rf /tmp/mako_lms
-echo "Reloading edX app\n"
+echo "Reloading edX app"
 sudo /edx/bin/supervisorctl restart edxapp:
 echo "Done"
