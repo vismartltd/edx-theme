79bce7fe65bdced13c346096de993dd1ffe53912
Alexander Khovansky <alx.khovansky@gmail.com>
2015-07-12 11:41:55 +0300

    Update theme script modified

--- updateTheme.sh
+++ updateTheme.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo -H -u edxapp bash -c 'source /edx/app/edxapp/edxapp_env;cd /edx/app/edxapp/edx-platform;paver update_assets lms --settings=aws'
+sudo -H -u edxapp bash -c 'git pull; source /edx/app/edxapp/edxapp_env;cd /edx/app/edxapp/edx-platform;paver update_assets lms --settings=aws'
 
 sudo rm -rf /tmp/mako_lms
 sudo /edx/bin/supervisorctl restart edxapp:
